3ca1de73284a94e1c6976849469d0b30f8aba2f7
k-yamada <yamadakazu45@gmail.com>
2016-10-14 10:34:31 +0900

    Initial Commit

---
+++ Kenban/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kenban
+//
+//  Created by 山田 和弘 on 2016/10/14.
+//  Copyright © 2016年 kyamada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kenban/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kenban
+//
+//  Created by 山田 和弘 on 2016/10/14.
+//  Copyright © 2016年 kyamada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
